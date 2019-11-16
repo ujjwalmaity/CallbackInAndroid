31eda74dff579136a5e5473eadc19216ce92addb
ujjwal <ujjwalmaity98@gmail.com>
2019-11-16 14:10:44 +0530

    Initial commit

---
+++ app/src/main/java/dev/ujjwal/callbackinandroid/CallBackActivity.java
@@ -0,0 +1,14 @@
+package dev.ujjwal.callbackinandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallBackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_back);
+    }
+}
